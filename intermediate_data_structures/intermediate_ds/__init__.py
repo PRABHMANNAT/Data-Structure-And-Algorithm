daea470b12be443c4f8f5452f2c1b47982657ee4
prabhmannat <23bcb10048@cuchd.in>
2026-09-17 23:38:29 +0530

    feat: expose core collection types

---
+++ intermediate_data_structures/intermediate_ds/__init__.py
@@ -0,0 +1,8 @@
+"""Readable, dependency-free data structure implementations."""
+
+from .dynamic_array import DynamicArray
+from .linked_list import LinkedList
+from .stack import Stack
+from .queue import Queue
+
+__all__ = ["DynamicArray", "LinkedList", "Stack", "Queue"]
