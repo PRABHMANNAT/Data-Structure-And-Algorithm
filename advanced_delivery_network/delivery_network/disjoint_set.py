36dc48aa52ddec3d414859cad56b31837f66f0cc
prabhmannat <23bcb10048@cuchd.in>
2026-09-17 23:47:02 +0530

    feat: add union find for route groups

---
+++ advanced_delivery_network/delivery_network/disjoint_set.py
@@ -0,0 +1,16 @@
+class DisjointSet:
+    def __init__(self, values=()):
+        self.parent = {}; self.rank = {}
+        for value in values: self.add(value)
+    def add(self, value):
+        if value not in self.parent: self.parent[value] = value; self.rank[value] = 0
+    def find(self, value):
+        if self.parent[value] != value: self.parent[value] = self.find(self.parent[value])
+        return self.parent[value]
+    def union(self, first, second):
+        first, second = self.find(first), self.find(second)
+        if first == second: return False
+        if self.rank[first] < self.rank[second]: first, second = second, first
+        self.parent[second] = first
+        if self.rank[first] == self.rank[second]: self.rank[first] += 1
+        return True
