4b541f7f838f3f8453cd2c0155812d0eafcff387
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:06:33 +0530

    test: normalize phrase query casing

--- versioned_search_engine/tests/test_phrase_search.py
+++ versioned_search_engine/tests/test_phrase_search.py
@@ -5,3 +5,5 @@ class PhraseTests(unittest.TestCase):
  def test_matches_adjacent_terms_only(self):
   i=InvertedIndex();i.add("a","fast train home");i.add("b","fast local train");self.assertEqual(phrase_matches(i,"fast train"),("a",))
  def test_empty_phrase_has_no_matches(self):self.assertEqual(phrase_matches(InvertedIndex(),""),())
+ def test_phrase_matching_is_case_insensitive(self):
+  i=InvertedIndex();i.add("a","Fast Train");self.assertEqual(phrase_matches(i,"FAST train"),("a",))
