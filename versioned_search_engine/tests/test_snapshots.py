380875848d6327fe4d9615fd55e306fabde08907
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:02:15 +0530

    test: expose captured snapshot count

--- versioned_search_engine/tests/test_snapshots.py
+++ versioned_search_engine/tests/test_snapshots.py
@@ -3,3 +3,5 @@ from search_engine.snapshots import SnapshotStore
 class SnapshotTests(unittest.TestCase):
  def test_restores_isolated_state_copy(self):
   s=SnapshotStore();state={"a":[1]};s.save(state);state["a"].append(2);self.assertEqual(s.restore(0),{"a":[1]})
+ def test_counts_captured_revisions(self):
+  s=SnapshotStore();s.save({});s.save({});self.assertEqual(len(s),2)
