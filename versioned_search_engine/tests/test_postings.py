44c31dfefa1da7574edb2d9801fea6c499dd2b6a
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:01:53 +0530

    test: delete document records from postings

--- versioned_search_engine/tests/test_postings.py
+++ versioned_search_engine/tests/test_postings.py
@@ -3,3 +3,5 @@ from search_engine.postings import PostingList
 class PostingsTests(unittest.TestCase):
  def test_tracks_sorted_document_ids_and_positions(self):
   p=PostingList();p.add("b",[2]);p.add("a",[1,3]);self.assertEqual((p.documents(),p.positions("a")),(("a","b"),(1,3)))
+ def test_removes_document_entry(self):
+  p=PostingList();p.add("a",[1]);p.remove("a");self.assertEqual(len(p),0)
