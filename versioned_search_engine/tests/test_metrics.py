09d4513a63cc93ac0336c28154c2d54e2fd66090
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:04:15 +0530

    test: default unseen metrics to zero

--- versioned_search_engine/tests/test_metrics.py
+++ versioned_search_engine/tests/test_metrics.py
@@ -3,3 +3,4 @@ from search_engine.metrics import Metrics
 class MetricsTests(unittest.TestCase):
  def test_counts_named_events(self):
   m=Metrics();m.inc("query");m.inc("query");self.assertEqual(m.get("query"),2)
+ def test_defaults_unknown_metric_to_zero(self):self.assertEqual(Metrics().get("missing"),0)
