634753cf26f2ee0f277d01a21a42f02561146ec3
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:02:21 +0530

    test: union alternative Boolean terms

--- versioned_search_engine/tests/test_boolean_search.py
+++ versioned_search_engine/tests/test_boolean_search.py
@@ -4,3 +4,5 @@ from search_engine.inverted_index import InvertedIndex
 class BooleanTests(unittest.TestCase):
  def test_combines_and_negates_term_postings(self):
   i=InvertedIndex();i.add("a","red blue");i.add("b","red");self.assertEqual(evaluate(i,("red","AND","NOT","blue"),("a","b")),("b",))
+ def test_unions_or_terms(self):
+  i=InvertedIndex();i.add("a","red");i.add("b","blue");self.assertEqual(evaluate(i,("red","OR","blue"),("a","b")),("a","b"))
