d71544e0ae71c6c07c51651827e3c9e7bff842f5
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 17:41:15 +0530

    test: guard against empty platform occupancy slots

--- algorithmic_transit_simulator/tests/test_intervals.py
+++ algorithmic_transit_simulator/tests/test_intervals.py
@@ -4,3 +4,5 @@ class IntervalTests(unittest.TestCase):
  def test_reports_only_overlapping_intervals(self):
   index=IntervalIndex(); index.add(Interval(10,20,"a")); index.add(Interval(30,40,"b"))
   self.assertEqual([x.value for x in index.conflicts(Interval(15,25,"new"))],["a"])
+ def test_rejects_empty_occupancy_interval(self):
+  with self.assertRaises(ValueError):IntervalIndex().add(Interval(4,4,"invalid"))
