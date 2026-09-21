b70a7fcc861e78cfeb9e4146f4239fe355668fb2
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:01:47 +0530

    test: remove stale terms from inverted index

--- versioned_search_engine/tests/test_inverted_index.py
+++ versioned_search_engine/tests/test_inverted_index.py
@@ -3,3 +3,5 @@ from search_engine.inverted_index import InvertedIndex
 class IndexTests(unittest.TestCase):
  def test_replaces_previous_document_terms(self):
   i=InvertedIndex();i.add("a","red blue");i.add("a","green");self.assertEqual(i.documents("red"),());self.assertEqual(i.documents("green"),("a",))
+ def test_removes_document_postings(self):
+  i=InvertedIndex();i.add("a","red");i.remove("a");self.assertEqual(i.documents("red"),())
