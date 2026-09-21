9d286ef0265ca13ca37b0c7e1f6a7295ef3d07be
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 18:00:51 +0530

    test: define empty phrase search behavior

--- versioned_search_engine/tests/test_phrase_search.py
+++ versioned_search_engine/tests/test_phrase_search.py
@@ -4,3 +4,4 @@ from search_engine.phrase_search import phrase_matches
 class PhraseTests(unittest.TestCase):
  def test_matches_adjacent_terms_only(self):
   i=InvertedIndex();i.add("a","fast train home");i.add("b","fast local train");self.assertEqual(phrase_matches(i,"fast train"),("a",))
+ def test_empty_phrase_has_no_matches(self):self.assertEqual(phrase_matches(InvertedIndex(),""),())
