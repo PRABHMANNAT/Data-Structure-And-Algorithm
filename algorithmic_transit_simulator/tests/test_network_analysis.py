b4afce5eb9734451d073a9eb12aba245b7faba94
prabhmannat <23bcb10048@cuchd.in>
2026-09-21 17:42:18 +0530

    test: identify isolated stops in network analysis

--- algorithmic_transit_simulator/tests/test_network_analysis.py
+++ algorithmic_transit_simulator/tests/test_network_analysis.py
@@ -1,7 +1,7 @@
 import unittest
 from transit_simulator.graph import TransitGraph
 from transit_simulator.models import Connection, Stop
-from transit_simulator.network_analysis import connected_components
+from transit_simulator.network_analysis import connected_components, isolated_stops
 
 class NetworkAnalysisTests(unittest.TestCase):
     def test_groups_stops_even_when_connection_is_one_way(self):
@@ -9,3 +9,7 @@ class NetworkAnalysisTests(unittest.TestCase):
         for x in "abc": graph.add_stop(Stop(x, x))
         graph.add_connection(Connection("ab", "a", "b", 1, 2, "L"))
         self.assertEqual({frozenset(x) for x in connected_components(graph)}, {frozenset("ab"), frozenset("c")})
+
+    def test_identifies_singleton_stop_as_isolated(self):
+        graph = TransitGraph(); graph.add_stop(Stop("solo", "Solo"))
+        self.assertEqual(isolated_stops(graph), {"solo"})
